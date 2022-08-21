6417800fb14c769f09b6ebcbdb31d446dd7a6739
Jinkyou Son <nallwhy@gmail.com>
2022-08-21 10:37:28 +0900

    Init Project

---
+++ lib/doumi/phoenix/svg.ex
@@ -0,0 +1,18 @@
+defmodule Doumi.Phoenix.SVG do
+  @moduledoc """
+  Documentation for `Doumi.Phoenix.SVG`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Doumi.Phoenix.SVG.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/doumi/phoenix/svg_test.exs
@@ -0,0 +1,8 @@
+defmodule Doumi.Phoenix.SVGTest do
+  use ExUnit.Case
+  doctest Doumi.Phoenix.SVG
+
+  test "greets the world" do
+    assert Doumi.Phoenix.SVG.hello() == :world
+  end
+end
