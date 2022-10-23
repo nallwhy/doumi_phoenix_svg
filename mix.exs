6ed70b389bc57a1c36fd14180ca19dbe9830b0d4
Jinkyou Son <nallwhy@gmail.com>
2022-10-24 01:53:36 +0900

    Bump up version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Doumi.Phoenix.SVG.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/nallwhy/doumi_phoenix_svg"
-  @version "0.2.0"
+  @version "0.3.0"
 
   def project do
     [
